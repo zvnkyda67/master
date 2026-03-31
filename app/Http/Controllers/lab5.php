401c02f8e0efee8ec6eda9e5ef7ad5688f9cc17e
Zankyda <zvnk@mail.com>
2026-03-31 04:30:15 -0700

    31.03.2026

--- app/Http/Controllers/lab5.php
+++ app/Http/Controllers/lab5.php
@@ -8,7 +8,18 @@ class lab5 extends Controller{
         return view('lab5.lab5', [
             'name'=>'Dasha',
             'age'=>'20',
-            'salary'=>'40000'
+            'salary'=>'40000',
+            'class'=>'body',
+            'var'=>'german',
+            'var1'=>'german1',
+            'var2'=>'german2',
+            'color'=>'color:red;',
+            'silka'=>'http://prof.ru/index.php',
+            'text'=>'Ссылка на prof.ru',
+            'arr'=>['name'=>'Sawa', 'age'=>'21','salary'=>'200000'],
+            'arr2'=>[1,2,3,4,5],
+            'city'=>'',
+            'location'=>['country'=>'', 'city1'=>'']
         ]);
     }
 
--- resources/views/lab5/lab5.blade.php
+++ resources/views/lab5/lab5.blade.php
@@ -2,7 +2,21 @@
     <x-slot:title>
         lab 5
     </x-slot:title>
-    {{$name}}
-    {{$age}}
-    {{$salary}}
+    <p>{{$name}}</p>
+    <p>{{$age}}</p>
+    <p>{{$salary}}</p>
+    <p class="{{ $class }}">badi</p>
+    <p><input value="{{ $var }}"></p>
+    <p><input value="{{ $var1 }}"></p>
+    <p><input value="{{ $var2 }}"></p>
+    <p style="{{ $color }}">Красни Текст</p>
+    <a href="{{ $silka }}">{{ $text }}</a>
+    <p>Текущая дата: {{ date('d.m.Y') }}</p>
+    <p>{{ $arr['name'] }}</p>
+    <p>{{ $arr['age'] }}</p>
+    <p>{{ $arr['salary'] }}</p>
+    <p>{{ count($arr2) }}</p>
+    <p>{{ $city?:'Moskov' }}</p>
+    <p>{{ $location['country']?:'Россия'}}</p>
+    <p>{{ $location['city1']?:'Омск' }}</p>
 </x-layout>
